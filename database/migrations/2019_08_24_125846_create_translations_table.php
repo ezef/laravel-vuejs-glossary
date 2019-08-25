6ebffb2bfd6c0102962e7f912945c2f7cc1c62ec
Ezequiel Flores <ezequielsebastianflores@gmail.com>
2019-08-25 19:44:38 -0300

    defining database tables

---
+++ database/migrations/2019_08_24_125846_create_translations_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTranslationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('translations', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('word');
+            $table->unsignedBigInteger('language_id');
+            $table->foreign('language_id')->references('id')->on('languages');
+            $table->unsignedBigInteger('term_id');
+            $table->foreign('term_id')->references('id')->on('terms');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('translations');
+    }
+}
